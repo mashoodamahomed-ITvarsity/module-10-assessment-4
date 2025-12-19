72222f07f63da6dae881f6b4fa5549c876c7c2bb
mashooda mahomed <mashoodaafsana@gmail.com>
2025-12-19 13:33:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("✨ Welcome to the Whimsical Gallery ✨");
+document.addEventListener("mousemove", function (event) {
+  const sparkle = document.createElement("div");
+  sparkle.classList.add("sparkle");
+
+  sparkle.style.left = event.pageX + "px";
+  sparkle.style.top = event.pageY + "px";
+
+  document.body.appendChild(sparkle);
+
+  setTimeout(function () {
+    sparkle.remove();
+  }, 600);
+});
